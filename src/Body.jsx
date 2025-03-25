37beef5882a0a72f21542ee2bf0e4c6923cdb8ce
VICTOR ZHOU <110187483+Xerof47Vel@users.noreply.github.com>
2025-03-25 09:48:11 +0200

    managed to differentiate the pnl colors to red and blue

--- src/Body.jsx
+++ src/Body.jsx
@@ -7,6 +7,8 @@ function Body(){
         <Card  title="NQ100" date={new Date().toDateString()} profitLoss={100} />
         <Card title="NQ100" date={new Date().toDateString()} profitLoss={100} />
         <Card title="NQ100" date={new Date().toDateString()} profitLoss={100} />
+        <Card title="NQ100" date={new Date().toDateString()} profitLoss={-100} />
+        <Card title="NQ100" date={new Date().toDateString()} profitLoss={100} />
         <Card title="NQ100" date={new Date().toDateString()} profitLoss={100} />
         <Card title="NQ100" date={new Date().toDateString()} profitLoss={100} />
         </div>
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,16 +1,36 @@
 import image from "./assets/Screenshot 2025-03-06 151914.png";
+import propTypes from "prop-types";
 function Card(props){
     //props take in values from the parent component in this case Body.jsx
     const imageAfter=image;
-
-
-    return(
+    if(props.profitLoss>=0){
+        return(
+       
+            <>
+      
+            <div className="card" onClick={()=>{console.log("Card Clicked")}}>
+                <img src={imageAfter} alt="after Trade image" className="tradeImage"/>
+                <h2 className="cardTittle">{props.title}</h2>
+                
+                <p className="profit">{"$"+props.profitLoss}</p>
+                <p className="dateTime">{props.date}</p>
+                
+    
+           
+            </div>
+            </>
+    
+        );
+    }
+    else{ return(
+       
         <>
-        
+  
         <div className="card" onClick={()=>{console.log("Card Clicked")}}>
             <img src={imageAfter} alt="after Trade image" className="tradeImage"/>
             <h2 className="cardTittle">{props.title}</h2>
-            <p className="profitLoss">{"$"+props.profitLoss}</p>
+            
+            <p className="loss">{"$"+props.profitLoss}</p>
             <p className="dateTime">{props.date}</p>
             
 
@@ -20,6 +40,15 @@ function Card(props){
 
     );
 
+    }
+
+   
+
+}
+Card.propTypes={
+    title:propTypes.string.isRequired,
+    date:propTypes.string.isRequired,
+    profitLoss:propTypes.number.isRequired
 }
 
 export default Card;
